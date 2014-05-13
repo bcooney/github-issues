e759bba3d417c2977ce12401f559d8d9ccb18f9c
Kieran <kieran@mpjdesign.ltd.uk>
2014-05-13 14:41:55 +0100

    Strip out pull requests, and include milestones

--- GithubIssues.php
+++ GithubIssues.php
@@ -19,7 +19,6 @@ echo 'Now tell us what repo you want to get issues for (e.g. unikent/of-course):
 $repo = fgets($handle);
 $ex   = explode('/', $repo);
 
-echo PHP_EOL;
 if (count($ex) !== 2) {
     throw new Exception('Invalid repository name');
     exit;
@@ -31,59 +30,90 @@ $auth = (object)$auth;
 
 // Authenticate (so that we can see private repos)
 $client    = new \Github\Client();
-$paginator = new Github\ResultPager($client);
-$client->authenticate(
-    $auth->username,
-    $auth->password,
-    Github\Client::AUTH_HTTP_PASSWORD
-);
+$paginator = new \Github\ResultPager($client);
 
-$issueApi = $client->api('issue');
-$issues   = [];
+try {
+    $client->authenticate(
+        $auth->username,
+        $auth->password,
+        \Github\Client::AUTH_HTTP_PASSWORD
+    );
 
-// Get the closed issues first - limited to 30
-$issues['closed'] = $issueApi->all(
-    $repo->owner,
-    $repo->name,
-    ['state' => 'closed']
-);
+    $issueApi = $client->api('issue');
+    $issues   = [];
 
-// Get all the open issues
-$issues['open']   = $paginator->fetchAll(
-    $issueApi,
-    'all',
-    [
+    // Get the closed issues first - limited to 30
+    $issues['closed'] = $issueApi->all(
         $repo->owner,
         $repo->name,
-        ['state' => 'open'],
-    ]
-);
+        ['state' => 'closed']
+    );
 
+    // Get all the open issues
+    $issues['open']   = $paginator->fetchAll(
+        $issueApi,
+        'all',
+        [
+            $repo->owner,
+            $repo->name,
+            ['state' => 'open'],
+        ]
+    );
+} catch (Github\Exception\RuntimeException $e) {
+    echo 'Error: ' . $e->getMessage();
+    exit;
+}
+
+echo PHP_EOL;
+echo sprintf(
+    '%s open and %s closed issues pulled…',
+    (string)count($issues['open']),
+    (string)count($issues['closed'])
+);
 
 // Set up the header rows for the CSV
 $toWrite = [
-    ['url', 'number', 'title', 'status', 'created_at', 'updated_at'],
+    ['url', 'number', 'title', 'status', 'assignee', 'milestone', 'created_at', 'updated_at'],
 ];
+
+$prCount = 0;
 
 // Set up the data rows for the CSV
 foreach ($issues as $states) {
     foreach ($states as $issue) {
+        // Strip out pull requests
+        if (strpos($issue['html_url'], '/pull/')) {
+            ++$prCount;
+            continue;
+        }
+
+        $assignee = $issue['assignee'] == null ? '' : $issue['assignee']['login'];
+        $milestone = $issue['milestone'] == null ? '' :
+            $issue['milestone']['number'] . ': ' . $issue['milestone']['title'];
+
         $toWrite[] = [
             $issue['html_url'],
             $issue['number'],
             $issue['title'],
             $issue['state'],
+            $assignee,
+            $milestone,
             $issue['created_at'],
             $issue['updated_at'],
         ];
     }
 }
 
-echo 'Writing to CSV…' . PHP_EOL;
+echo PHP_EOL;
+echo sprintf('Stripping out %s pull requests…', (string)$prCount);
+
+echo PHP_EOL;
+echo 'Writing to CSV…';
 
 // Write the array to the CSV file
-$writer = new Writer(sprintf('%s.csv', $repo->name), 'ab+');
+$writer = new Writer(sprintf('%s.csv', $repo->owner . '_' . $repo->name), 'ab+');
 $writer->setNullHandlingMode(Writer::NULL_AS_EMPTY);
 $writer->insertAll($toWrite);
 
+echo PHP_EOL;
 echo sprintf('Done! CSV file is %s.csv', $repo->name);
